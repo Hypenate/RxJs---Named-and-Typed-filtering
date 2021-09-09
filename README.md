3800379f10bf527c1eff8e98b8d4f1e31786686e
Hypenate <ben.beckers@gmail.com>
2021-09-09 10:57:37 +0200

    3 files modified

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Case
-Shows way to name and/or type variables in the RxJs filter.
-
-# rxjs-wq7ew8
+Show ways to name and/or type variables in the RxJs filter.
+Also shows how to "drop" variables or group them using the spread operator.
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-wq7ew8)
--- index.ts
+++ index.ts
@@ -2,12 +2,16 @@ import './style.css';
 import { of, combineLatest } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
-// Can be null or string for example purposes.
-const parameters$ = combineLatest<[string | null, string | null]>([
+// Example Observable.
+const parameters$ = combineLatest<
+  [string | null, string | null, string | null]
+>([
   of<string | null>('foo'),
+  of<string | null>('bar'),
   of<string | null>(null)
 ]);
 
+// #### Naming and/or typing parameers
 // Named parameters (foo and bar).
 const namedParameters$ = parameters$.pipe(
   filter(([foo, bar]) => foo !== null && bar !== null)
@@ -15,10 +19,37 @@ const namedParameters$ = parameters$.pipe(
 
 // Typed parameters (string and string).
 const typedParameters$ = parameters$.pipe(
-  filter((params): params is [string, string] => !params[0].includes(null))
+  filter((params): params is [string, string, string] => !params.includes(null))
 );
 
 // Named and typed parameters.
 const namedTypedParameters$ = parameters$.pipe(
-  filter(([foo, bar]: [string, string]) => foo !== null && bar !== null)
+  filter(
+    ([foo, bar, other]: [string, string, string]) =>
+      foo !== null && bar !== null && other !== null
+  )
+);
+
+// #### Dropping values ####
+// Named and typed parameters, only keep first item.
+const fooNamedTypedParameters$ = parameters$.pipe(
+  filter(([foo]: [string, string, string]) => foo !== null)
+);
+
+// Named and typed parameters, only keep second item.
+const barNamedTypedParameters$ = parameters$.pipe(
+  filter(([, bar]: [string, string, string]) => bar !== null)
+);
+
+// Named and typed parameters, only keep third item.
+const otherNamedTypedParameters$ = parameters$.pipe(
+  filter(([, , other]: [string, string, string]) => other !== null)
+);
+
+// Named and typed parameters, but drop some using the spread operator.
+const spreadNamedTypedParameters$ = parameters$.pipe(
+  filter(
+    ([foo, ...others]: [string, ...string[]]) =>
+      foo !== null && others.includes(null)
+  )
 );
