f4aeb0a0355d87ea60b602598cbbe0d0ff7e341f
Hypenate <ben.beckers@gmail.com>
2021-09-09 10:07:58 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import './style.css';
-
-import { of, map, combineLatest, BehaviorSubject } from 'rxjs';
+import { of, combineLatest } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
-// Can be null or string for example purpose.
+// Can be null or string for example purposes.
 const parameters$ = combineLatest([
   of<string | null>('foo'),
   of<string | null>(null)
@@ -14,17 +13,12 @@ const namedParameters$ = parameters$.pipe(
   filter(([foo, bar]) => foo !== null && bar !== null)
 );
 
-// Named parameters (foo and bar) in a desctructed manner.
-const destructedNamedParameters$ = parameters$.pipe(
-  filter(([foo, bar]) => foo !== null && bar !== null)
-);
-
-// Typed parameters (string and string)
+// Typed parameters (string and string).
 const typedParameters$ = parameters$.pipe(
   filter((params): params is [string, string] => !params[0].includes(null))
 );
 
-// Named and typed parameters (foo: string and bar: string
+// Named and typed parameters.
 const namedTypedParameters$ = parameters$.pipe(
   filter(([foo, bar]: [string, string]) => foo !== null && bar !== null)
 );
